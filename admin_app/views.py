4f970910aa77ee3487e33e6b472eda6eed49e2bd
andre <andresalexanderhq@gmail.com>
2024-12-10 06:31:03 -0500

    relizar prueba con docker

--- admin_app/views.py
+++ admin_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+        
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'El nombre de usuario ya existe'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user = User.objects.create(
+            username=username,
+            email=email,
+            password=make_password(password)
+        )
+        return Response({'message': 'Usuario registrado con éxito'}, status=status.HTTP_201_CREATED)
--- admin_project/settings.py
+++ admin_project/settings.py
@@ -92,10 +92,19 @@ SIMPLE_JWT = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nowwatch',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+DATABASE_ROUTERS = ['routers.ExistingDatabaseRouter']
 
 
 # Password validation
